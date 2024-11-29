ad589166e3d035bc36bc170d7d59fa5073e6796b
AlexisWeiss <aweiss@ccld.com>
2024-11-29 12:03:44 +0100

    cryptage ok, ménage dans le code pour séparer les fontion

--- Emission/source/main.cpp
+++ Emission/source/main.cpp
@@ -1,113 +1,128 @@
 /*Le but de la carte BRD-C-828 est d'émmèttre les données reçu sur son port série à la carte numéro 1 en liaison radio */
-//AlexisWEISS
+//Alexis
 #include <MicroBit.h>
 #include <stdio.h>
-
+#define CLECRYPTAGE 0x4c // Clé XOR
 MicroBit uBit;
 
 /*=====================================FONCTIONS===========================================*/
+
+    bool process_fire(ManagedString fireData, ManagedString &blockMessage, int &fireCount, int delay) {
+        int x = 0, y = 0, i = 0, t = 0;
+
+        // Vérifie le format du feu
+        if (sscanf(fireData.toCharArray(), "(%d,%d,%d,%d)", &x, &y, &i, &t) == 4) {
+            // Ajoute le feu au bloc
+            blockMessage = blockMessage + fireData;
+            fireCount++;
+            return true; // Feu traité avec succès
+        } else {
+            // Si un feu est mal formé, afficher une erreur
+            uBit.display.scroll("ERR FIRE", delay);
+            return false; // Erreur de format
+        }
+    }
+
+        ManagedString xorEncryptDecrypt(ManagedString blockMessage) {
+        ManagedString result;
+        for (int i = 0; i < blockMessage.length(); i++) {
+            char encryptedChar = blockMessage.charAt(i) ^ CLECRYPTAGE;
+            result = result + encryptedChar;
+        }
+        return result;
+        }
+
+    void send_block_if_full(ManagedString &blockMessage, int &fireCount, int delay, bool isLast) { 
+        if (fireCount == 10) {
+            if (isLast) {
+                blockMessage = blockMessage + "LAST"; // Ajoute "LAST" au dernier bloc
+            }
+            uBit.display.scroll(blockMessage, delay);
+            uBit.radio.datagram.send(xorEncryptDecrypt(blockMessage)); // Envoi du bloc en la cryptant
+            uBit.display.scroll("TX BLOCK", delay);
+            blockMessage = "";   // Réinitialise le bloc
+            fireCount = 0;       // Réinitialise le compteur
+        }
+    }
+
+    void send_last_block(ManagedString &blockMessage, int &fireCount, int delay) {
+        if (fireCount > 0) {
+            uBit.display.scroll(blockMessage.length(), delay); // Affiche la longueur du message pour débogage
+            blockMessage = blockMessage + "LAST";             // Ajoute LAST à la fin
+            uBit.display.scroll(blockMessage, delay);         // Affiche le message pour débogage
+            uBit.radio.datagram.send(xorEncryptDecrypt(blockMessage));           // Envoi du bloc en le cryptant
+            uBit.display.scroll("TX LAST", delay);            // Affiche confirmation d’envoi
+            blockMessage = "";   // Réinitialise le bloc
+            fireCount = 0;       // Réinitialise le compteur
+        }
+    }
 
 
 
 /*=====================================MAIN===========================================*/
 
-int main() {
-    int delay = 60;
-    uBit.display.scroll("init",delay);
-    uBit.init();
+    int main() {
+        int delay = 60;
+        uBit.display.scroll("init",delay);
+        uBit.init();
 
-    uBit.radio.enable();
-    uBit.radio.setGroup(76); // Groupe radio pour éviter les interférences
+        uBit.radio.enable();
+        uBit.radio.setGroup(76); // Groupe radio pour éviter les interférences
 
-    uBit.serial.baud(115200);
-    uBit.serial.setRxBufferSize(248); // Taille du tampon de réception
+        uBit.serial.baud(115200);
+        uBit.serial.setRxBufferSize(248); // Taille du tampon de réception
 
-        while (true) {
-            uBit.display.scroll("waiting", delay);
-
-            // Vérifie s'il y a des données disponibles sur le port série
-            if (uBit.serial.isReadable()) {
-                uBit.display.scroll("Reading", delay);
-
-                // Lit les données sur le port série
-                ManagedString receivedData = uBit.serial.readUntil('\n'); // Lit jusqu'à une nouvelle ligne
-                uBit.display.scroll("Stop Reading", delay);
-                uBit.display.scroll(receivedData, delay);
-
-                int start = 0;               // Position de début pour parcourir le message (numero de capteur)
+            while (true) {
+                uBit.display.scroll("waiting", delay);
                 int fireCount = 0;           // Compte le nombre de feux dans un bloc
                 ManagedString blockMessage="";  // Stocke les feux dans un bloc de 10 pour envoi radio
                 bool isLast = false;         // Indique si le dernier bloc est atteint
-                uBit.display.scroll(receivedData.length(), delay);
 
-                // Parcourt chaque feu dans le message
-                while (start < receivedData.length()) {
-                    int end = start;
-                    for (int i = start; i < receivedData.length(); i++) {
-                        if (receivedData.charAt(i) == ')') {
-                            end = i;
-                            break;
+                if (uBit.serial.isReadable()) { // Vérifie s'il y a des données disponibles sur le port série
+
+                    uBit.display.scroll("Reading", delay);
+
+                    ManagedString receivedData = uBit.serial.readUntil('\n'); // Lit les données sur le port série
+
+                    uBit.display.scroll("Stop Reading", delay);
+                    int start = 0;// Position de début pour parcourir le message (numero de capteur)
+                    
+                    uBit.display.scroll(receivedData.length(), delay);//DEBUG
+
+                    while (start < receivedData.length()) { // Parcourt chaque feu dans le message
+
+                        int end = start;
+                        for (int i = start; i < receivedData.length(); i++) {
+                            if (receivedData.charAt(i) == ')') {
+                                end = i;
+                                break;
+                            }
                         }
-                    }
+                        
+                        if (end == start) break;// Si aucune parenthèse fermante trouvée
 
-                    // Si aucune parenthèse fermante trouvée
-                    if (end == start) break;
+                        ManagedString fireData = receivedData.substring(start, end+1-start);// Extraire un feu complet
+                        start = end + 1; // Passe au prochain feu
 
-                    // Extraire un feu complet
-
-                    ManagedString fireData = receivedData.substring(start, end+1-start);
-                    start = end + 1; // Passe au prochain feu
-
-                    // Variables pour stocker les données du feu
-                    int x = 0, y = 0, i = 0, t = 0;
-
-                    // Vérifie le format du feu
-                    if (sscanf(fireData.toCharArray(), "(%d,%d,%d,%d)", &x, &y, &i, &t) == 4) {
-                        // Ajoute le feu au bloc
-                        blockMessage = blockMessage + fireData;
-                        fireCount++;
-                    } else {
-                        // Si un feu est mal formé, afficher une erreur et ignorer ce feu
-                        uBit.display.scroll("ERR FIRE", delay);
-                        continue;
-                    }
-
-                    // Vérifie si on a atteint le dernier feu
-                    isLast = (start >= receivedData.length());
-                    uBit.display.scroll(fireCount, delay);
-
-                    // Si le bloc contient 10 feux, l'envoyer
-                    if (fireCount == 10) {
-                        if (isLast) {
-                            blockMessage = blockMessage + "LAST"; // Ajoute "LAST" au dernier bloc
+                        if (!process_fire(fireData, blockMessage, fireCount, delay)) {
+                            continue; // Ignore le feu en cas d'erreur
                         }
-                        uBit.display.scroll(blockMessage, delay);
-                        uBit.radio.datagram.send(blockMessage); // Envoi du bloc
-                        uBit.display.scroll("TX BLOCK", delay);
-                        blockMessage = "";   // Réinitialise le bloc
-                        fireCount = 0;       // Réinitialise le compteur
+
+                        isLast = (start >= receivedData.length());// Vérifie si on a atteint le dernier feu
+                        uBit.display.scroll(fireCount, delay);//DEBUG
+                        send_block_if_full(blockMessage, fireCount, delay, isLast);// Si le bloc contient 10 feux, l'envoyer
+
                     }
-                    uBit.display.scroll(blockMessage.length(), delay);
+                        uBit.display.scroll(blockMessage.length(), delay);//DEBUG
                 }
 
                 // Envoyer les feux restants dans le dernier bloc (moins de 10 feux)
-                if (fireCount > 0) {
-                    uBit.display.scroll(blockMessage.length(), delay);
-                    blockMessage = blockMessage + "LAST"; // Ajoute LAST à la fin
-                    uBit.display.scroll(blockMessage, delay);
-                    uBit.radio.datagram.send(blockMessage); // Envoi du dernier bloc
-                    uBit.display.scroll("TX LAST", delay);
-                    blockMessage = "";   // Réinitialise le bloc
-                    fireCount = 0;       // Réinitialise le compteur
-                }
+                send_last_block(blockMessage, fireCount, delay);
+
             }
 
-        // Petite pause pour éviter une boucle trop rapide
-        uBit.sleep(100);
-        printf("pause de 100ms");
+            // Petite pause pour éviter une boucle trop rapide
+            uBit.sleep(100);
+        
+        release_fiber();
     }
-
-
-
-    release_fiber();
-}
--- Reception/source/main.cpp
+++ Reception/source/main.cpp
@@ -1,6 +1,6 @@
 //AlexisWEISS
 #include "MicroBit.h"
-
+#define CLECRYPTAGE 0x4c // Clé XOR
 MicroBit uBit;
 
 // Variables globales pour stocker les données reçues
@@ -13,42 +13,54 @@ int delay = 70;
 ManagedString fullMessage = ""; // Stocke le message complet reconstitué
 int blockCount = 0;             // Compteur de blocs reçus
 
-void onData(MicroBitEvent) {
-    // Réception d'un bloc de feux par radio
-    ManagedString block = uBit.radio.datagram.recv();
-    fullMessage = fullMessage + block; // Ajout du bloc au message complet
-    blockCount++;
+/*=====================================FONCTIONS===========================================*/
 
-    // Affichage pour le débogage
-    uBit.display.scroll("RX BLOCK", delay);
-
-    // Vérifiez si tous les blocs sont reçus
-    // Exemple ici : Attendez un maximum de 6 blocs ou un bloc contenant "LAST"
-    if (blockCount == 6 || block.substring(block.length() - 4, block.length()) == "LAST") {
-        uBit.display.scroll("RX COMPLETE", delay); // Débogage : Message complet reçu
-        uBit.display.scroll(fullMessage, delay);  // Affiche le message complet pour vérification
-
-        // Réinitialise les variables pour le prochain message
-        fullMessage = "";
-        blockCount = 0;
+    ManagedString xorEncryptDecrypt(ManagedString blockMessage) {
+    ManagedString result;
+    for (int i = 0; i < blockMessage.length(); i++) {
+        char encryptedChar = blockMessage.charAt(i) ^ CLECRYPTAGE;
+        result = result + encryptedChar;
     }
-}
+    return result;
+    }
 
+    void onData(MicroBitEvent) {
+        // Réception d'un bloc de feux par radio
+        ManagedString block = uBit.radio.datagram.recv();
+        ManagedString uncrypted_block = xorEncryptDecrypt(block);   
+        fullMessage = fullMessage + uncrypted_block; // Ajout du bloc au message complet
+        blockCount++;
 
+        // Affichage pour le débogage
+        uBit.display.scroll("RX BLOCK", delay);
 
-int main()
-{
-    // Initialise le runtime de la micro:bit
-    uBit.init();
+        // Vérifiez si tous les blocs sont reçus
+        // Exemple ici : Attendez un maximum de 6 blocs ou un bloc contenant "LAST"
+        if (blockCount == 6 || uncrypted_block.substring(uncrypted_block.length() - 4, uncrypted_block.length()) == "LAST") {
+            uBit.display.scroll("RX COMPLETE", delay); // Débogage : Message complet reçu
+            uBit.display.scroll(fullMessage, delay);  // Affiche le message complet pour vérification
 
-    // Configure le groupe radio (doit être identique à celui de la carte émettrice)
-    uBit.radio.setGroup(76); 
-    // Écoute les événements de réception radio
-    uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);
-    uBit.radio.enable();
+            // Réinitialise les variables pour le prochain message
+            fullMessage = "";
+            blockCount = 0;
+        }
+    }
 
-    // Boucle principale
-    while (1)
-        uBit.sleep(1000);
+/*=====================================MAIN===========================================*/
 
-}
+    int main()
+    {
+        // Initialise le runtime de la micro:bit
+        uBit.init();
+
+        // Configure le groupe radio (doit être identique à celui de la carte émettrice)
+        uBit.radio.setGroup(76); 
+        // Écoute les événements de réception radio
+        uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);
+        uBit.radio.enable();
+
+        // Boucle principale
+        while (1)
+            uBit.sleep(1000);
+
+    }
